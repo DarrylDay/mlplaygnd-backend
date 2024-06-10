f272ae0afa726e3e8f20cf437cb982919deb22fa
Darryl Day <darrylday16@gmail.com>
2024-06-10 15:59:02 -0700

    Check auth and return full prediction object

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS 
+from backend import supabase
 from mnist.predict import get_prediction
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
+import uuid
 
 DEBUG = os.getenv("DEBUG") == "True"
 HOST = os.getenv("FLASK_HOST")
@@ -15,11 +17,36 @@ CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+
+    if request.authorization is None or request.authorization.token is None:
+        return "Authorization token not found", 400
+
+    jwt = request.authorization.token
+
+    try:
+        user = supabase.auth.get_user(jwt).user
+    except Exception as e:
+        print(e)
+        return "Invalid authorization token", 400
+    except:
+        return "Invalid authorization token", 400
+
     if request.method == "POST":
         file = request.files['file']
         img_bytes = file.read()
         prediction = get_prediction(img_bytes)
-        return jsonify({'prediction': prediction})
+
+        bucket = "mnist_images"
+        fileName = f"{uuid.uuid4()}.png"
+        filePath = f"{user.id}/{fileName}"
+        supabase.storage.from_(bucket).upload(filePath, img_bytes, {
+            "content-type": "image/png"
+        })
+        res = supabase.table('mnist_predictions').insert(
+            {"user_id": user.id, "prediction": int(prediction), "img_bucket": bucket, "img_filename": fileName}).execute()
+        
+        return jsonify(res.data[0])
+        # return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, host=HOST, port=PORT)
